7187c438bfb0d8dc6853c2c692be64777dac1c38
Jeans <sistemadesignstyle@gmail.com>
2025-08-31 18:00:10 -0500

    actualizando logo

--- dist/proy_test/server/main.server.mjs
+++ dist/proy_test/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-HKDHC2XC.mjs";import"./chunk-CI2Q5XLP.mjs";import"./chunk-SHWZYJIB.mjs";import"./chunk-JCKR2SAN.mjs";import"./chunk-57NACS4Z.mjs";import"./chunk-4ZLH4SZV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-VD6DSEYJ.mjs";import"./chunk-CI2Q5XLP.mjs";import"./chunk-2GLBRZ3C.mjs";import"./chunk-JCKR2SAN.mjs";import"./chunk-57NACS4Z.mjs";import"./chunk-4ZLH4SZV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
